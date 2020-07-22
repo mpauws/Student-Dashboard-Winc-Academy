8eb56087d2ca9a74f96b439382eccdc5df6be9b1
Marco Pauws <mpauws@gmail.com>
2020-07-22 22:24:00 +0200

    initial commit: research stage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import studentEvaluationData from "./orientation/data-structure/data-structure";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   // const [students] = useState(studentEvaluationData);
+
+   // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+   // Sort Functionality
+   const sortedStudentEvaluationData = studentEvaluationData.sort((student1, student2) => {
+      if (student1.name > student2.name) {
+         return 1;
+      } else if (student1.name === student2.name) {
+         return 0;
+      } else if (student1.name < student2.name) {
+         return -1;
+      }
+      return 0;
+   });
+
+   // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+   // Get ordered list of students
+   const getStudentList = sortedStudentEvaluationData.map((student) => <li key={student.id}>{student.name}</li>);
+
+   // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+   // Get list of assignments
+   const renderAssignments = studentEvaluationData.map((student) => {
+      return student.assignment.map((assignment) => {
+         return assignment.name;
+      });
+   });
+   // .join(" ");
+
+   const copyAllAssignments = [];
+
+   const makeOneAssignmentArray = renderAssignments.forEach(function (assignment) {
+      copyAllAssignments.push(assignment);
+   });
+
+   console.log(renderAssignments);
+   console.log(makeOneAssignmentArray);
+
+   return (
+      <div className="app-container">
+         <h2>Students</h2>
+         <ul>{getStudentList ? getStudentList : null}</ul>
+         <h2>Assignments</h2>
+         <p></p>
+         <ul>{renderAssignments ? renderAssignments : null}</ul>
+      </div>
+   );
 }
 
 export default App;
+
+/* 
+const data = {
+   payload: [
+      {
+         id: 1,
+         name: "Atta",
+         brands: [
+            {
+               id: 118,
+               name: "Wheatola",
+               subProducts: [
+                  {
+                     id: 858,
+                     name: "Chakki Aata",
+                     minPrice: 52,
+                     maxPrice: 56,
+                  },
+                  {
+                     id: 2,
+                     name: "Chakki Atta",
+                     minPrice: 222,
+                     maxPrice: 236,
+                  },
+               ],
+            },
+         ],
+      },
+      {
+         id: 16,
+         name: "Rice (Branded)",
+         brands: [
+            {
+               id: 25,
+               name: "CookStar",
+               subProducts: [
+                  {
+                     id: 1163,
+                     name: "Best Basmati",
+                     creditDays: 0,
+                     minPrice: 5600,
+                     maxPrice: 5600,
+                  },
+                  {
+                     id: 863,
+                     name: "Extra Long Grain Basmati",
+                     creditDays: 0,
+                     minPrice: 7800,
+                     maxPrice: 7800,
+                  },
+               ],
+            },
+         ],
+      },
+   ],
+};
+
+const renderData = data.payload
+   .map((payload) => {
+      return payload.brands
+         .map((brand) => {
+            return brand.subProducts
+               .map((subProduct) => {
+                  return `${payload.name}, ${brand.name}, ${subProduct.name}`;
+               })
+               .join("\n");
+         })
+         .join("\n");
+   })
+   .join("\n"); */
---
+++ src/orientation/data-structure/data-structure.js
@@ -0,0 +1,39 @@
+// [[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]
+// Data structure experiments
+
+const studentEvaluationData = [
+   {
+      id: 1,
+      name: "Peter Timofeeff",
+      assignment: [
+         {
+            name: "W1D1",
+            enjoymentRating: 3,
+            difficultyRating: 4,
+         },
+         {
+            name: "W1D2",
+            enjoymentRating: 5,
+            difficultyRating: 3,
+         },
+      ],
+   },
+   {
+      id: 2,
+      name: "Helga van Leur",
+      assignment: [
+         {
+            name: "W1D1",
+            enjoymentRating: 1,
+            difficultyRating: 5,
+         },
+         {
+            name: "W1D2",
+            enjoymentRating: 5,
+            difficultyRating: 1,
+         },
+      ],
+   },
+];
+
+export default studentEvaluationData;
