ffc2f5b49da99229fde91199646fcf5ffbe07e65
Marco Pauws <mpauws@gmail.com>
2020-07-23 20:13:45 +0200

    updated component structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
 import StudentOverview from "./components/StudentOverview";
 import Dashboard from "./components/Dashboard";
+import Footer from "./components/Footer";
 
 // Import raw student evaluation data
 import studentEvaluationData from "./data/student-evaluation-data";
@@ -14,11 +15,8 @@ import "./App.css";
 function App() {
    // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
    // Get list of students
-   console.log(studentEvaluationData.map((x) => x.student));
-   const getStudentList = [...new Set(studentEvaluationData.map((x) => x.student))];
-   // console.log("getStudentList: ", getStudentList);
 
-   const sortedStudentList = getStudentList.sort((student1, student2) => {
+   const getStudentList = [...new Set(studentEvaluationData.map((x) => x.student))].sort((student1, student2) => {
       if (student1 > student2) {
          return 1;
       } else if (student1 === student2) {
@@ -28,92 +26,28 @@ function App() {
       }
       return 0;
    });
+   // console.log("getStudentList: ", getStudentList);
+   /* Output: ["Diana Woei", "Erwin Kroll", "Gerrit Hiemstra", "Helga van Leur", "Marco Verhoef", "Marjon de Hond", "Monique Somers", "Peter Timofeeff", "Piet Paulusma", "Steven Seagal"] */
 
    // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
    // Get list of assignments
-
-   const getAssignments = [...new Set(studentEvaluationData.map((x) => x.assignment))];
-
-   const sortedAssignmentList = getAssignments.sort((assignment1, assignment2) => {
-      if (assignment1 > assignment2) {
-         return 1;
-      } else if (assignment1 === assignment2) {
+   const getAssignments = [...new Set(studentEvaluationData.map((x) => x.assignment))].sort(
+      (assignment1, assignment2) => {
+         if (assignment1 > assignment2) {
+            return 1;
+         } else if (assignment1 === assignment2) {
+            return 0;
+         } else if (assignment1 < assignment2) {
+            return -1;
+         }
          return 0;
-      } else if (assignment1 < assignment2) {
-         return -1;
       }
-      return 0;
-   });
-
-   // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
-   // DRY function for accessing enjoyment rating for student x
-
-   const getEnjoymentRatings = (student, assignment) => {
-      return studentEvaluationData
-         .filter((item) => {
-            return item.student === student;
-         })
-         .filter((student) => {
-            return student.assignment === assignment;
-         })
-         .map((assignment) => assignment.enjoymentRating);
-   };
-
-   console.log(
-      "Enjoyment Rating van Piet Paulusma voor W1D2-1 (DRY): ",
-      getEnjoymentRatings("Piet Paulusma", "W1D2-1")
    );
 
-   // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
-   // DRY function for accessing difficulty rating for student x
+   // console.log("getAssignments: ", getAssignments);
+   /* Output: ["W1D1-1", "W1D2-1", "W1D2-2", "W1D2-3", "W1D2-4", "W1D2-5", "W1D3-1", "W1D3-2", "W1D3-4", "W1D3-5", "W1D3-6", "W1D4-1", "W1D4-2", "W1D5-1", "W2D1-1", "W2D1-2", "W2D2-1", "W2D2-2", "W2D2-3", "W2D3-1", "W2D3-2", "W2D3-3", "W2D4-1", "W2D4-2", "W2D4-3", "W2D5-1", "W3D1-1", "W3D1-2", "W3D1-3", "W3D1-4", "W3D2-1", "W3D2-2", "W3D2-3", "W3D3-1", "W3D3-2", "W3D3-3", "W3D3-4", "W3D4-1", "W3D4-2", "W3D5-1", "W4D2-1", "W4D2-2", "W4D2-3", "W4D2-4", "W4D2-5", "W4D3-1", "W4D3-2", "W4D3-3", "W4D3-4", "W4D3-5", "W4D3-6", "W5D4-1", "W5D5-1", "W6D1-1", "W6D2-1"] */
 
-   const getDifficultyRatings = (student, assignment) => {
-      return studentEvaluationData
-         .filter((item) => {
-            return item.student === student;
-         })
-         .filter((student) => {
-            return student.assignment === assignment;
-         })
-         .map((assignment) => assignment.difficultyRating);
-   };
-
-   console.log(
-      "Difficulty Rating van Piet Paulusma voor W1D2-1 (DRY): ",
-      getDifficultyRatings("Piet Paulusma", "W1D2-1")
-   );
-
-   // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
-   // DRY function for calculating average difficultyRating
-   const getAverageDifficultyRating = (assignment) => {
-      const getTotalDifficultyRating = studentEvaluationData
-         .filter((item) => {
-            return item.assignment === assignment;
-         })
-         .map((item) => item.difficultyRating)
-         .reduce((currentTotal, grading) => {
-            return grading + currentTotal;
-         }, 0);
-      return getTotalDifficultyRating / getStudentList.length;
-   };
-
-   console.log("getAverageDifficultyRating van W5D4-1", getAverageDifficultyRating("W5D4-1"));
-
-   // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
-   // DRY function for calculating average difficultyRating
-   const getAverageEnjoymentRating = (assignment) => {
-      const getTotalEnjoymentRating = studentEvaluationData
-         .filter((item) => {
-            return item.assignment === assignment;
-         })
-         .map((item) => item.enjoymentRating)
-         .reduce((currentTotal, grading) => {
-            return grading + currentTotal;
-         }, 0);
-      return getTotalEnjoymentRating / getStudentList.length;
-   };
-
-   console.log("getAverageEnjoymentRating van W5D4-1", getAverageEnjoymentRating("W5D4-1"));
+  
 
    // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
    // Return
@@ -123,9 +57,14 @@ function App() {
          <div className="app-container">
             <NavBar />
             <Switch>
-               <Route path="/" exact component={Dashboard} />
-               <Route path="/studentoverview" component={StudentOverview} />
+               <Route path="/" exact>
+                  <Dashboard getStudentList={getStudentList} getAssignments={getAssignments} />
+               </Route>
+               <Route path="/studentoverview">
+                  <StudentOverview getStudentList={getStudentList} getAssignments={getAssignments} />
+               </Route>
             </Switch>
+            <Footer />
          </div>
       </Router>
    );
@@ -133,29 +72,7 @@ function App() {
 
 export default App;
 
-/* return (
-   <Router>
-      <div className="app-container">
-         <nav>
-            <ul>
-               <li>
-                  <Link to="/">Dashboard</Link>
-               </li>
-               <li>
-                  <Link to="/studentoveriew">Student Overview</Link>
-               </li>
-            </ul>
-         </nav>
-         <main>
-            <Switch>
-               <Route path="/studentoverview">
-                  <StudentOverview />
-               </Route>
-               <Route path="/">
-                  <Dashboard />
-               </Route>
-            </Switch>
-            {/*                <h2>Students</h2>
+/*                <h2>Students</h2>
             <ul>
                {sortedStudentList ? sortedStudentList.map((student) => <li key={student}>{student}</li>) : null}
             </ul>
@@ -165,10 +82,7 @@ export default App;
                   : null}
             </ul>
 }
-         </main>
-      </div>
-   </Router>
-); */
+        
 
 // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 // Developer Functions
@@ -186,7 +100,6 @@ export default App;
 
    console.log("Enjoyment Rating van Piet Paulusma voor W1D2-1: ", getEnjoyRatingPietW1D2_1);
 */
-
 // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
 // Get average difficultyRating of W5D4-1
 /* 
@@ -202,3 +115,30 @@ export default App;
 
    const getAverageDifficultyRatingW5D4_1 = getTotalDifficultyRatingW5D4_1 / getStudentList.length;
    console.log("getAverageDifficultyRatingW5D4_1: ", getAverageDifficultyRatingW5D4_1); */
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+// Sort studentlist
+/*    const sortedStudentList = getStudentList.sort((student1, student2) => {
+      if (student1 > student2) {
+         return 1;
+      } else if (student1 === student2) {
+         return 0;
+      } else if (student1 < student2) {
+         return -1;
+      }
+      return 0;
+   }); */
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+// Sort assignments
+/* const sortedAssignmentList = getAssignments.sort((assignment1, assignment2) => {
+   if (assignment1 > assignment2) {
+      return 1;
+   } else if (assignment1 === assignment2) {
+      return 0;
+   } else if (assignment1 < assignment2) {
+      return -1;
+   }
+   return 0;
+});
+ */
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,48 @@
 import React from "react";
 import "../App.css";
 
-function Dashboard() {
-   //
+import studentEvaluationData from "../data/student-evaluation-data";
+
+function Dashboard({ getStudentList, getAssignments }) {
+   // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+   // DRY function for calculating average difficultyRating
+   const getAverageDifficultyRating = (assignment) => {
+      const getTotalDifficultyRating = studentEvaluationData
+         .filter((item) => {
+            return item.assignment === assignment;
+         })
+         .map((item) => item.difficultyRating)
+         .reduce((currentTotal, grading) => {
+            return grading + currentTotal;
+         }, 0);
+      return getTotalDifficultyRating / getStudentList.length;
+   };
+
+   console.log("getAverageDifficultyRating van W5D4-1", getAverageDifficultyRating("W5D4-1"));
+
+   // [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+   // DRY function for calculating average difficultyRating
+   const getAverageEnjoymentRating = (assignment) => {
+      const getTotalEnjoymentRating = studentEvaluationData
+         .filter((item) => {
+            return item.assignment === assignment;
+         })
+         .map((item) => item.enjoymentRating)
+         .reduce((currentTotal, grading) => {
+            return grading + currentTotal;
+         }, 0);
+      return getTotalEnjoymentRating / getStudentList.length;
+   };
+
+   console.log("getAverageEnjoymentRating van W5D4-1", getAverageEnjoymentRating("W5D4-1"));
 
    return (
-      <div>
+      <div className="component-container">
          <h2>Dashboard</h2>
+         <p className="page-description">
+            Welcome to the dashboard section. The graph below displays the average ratings of all Winc students. Head
+            over the student overview for the individual difficulty and enjoyment ratings given by the students.
+         </p>
       </div>
    );
 }
